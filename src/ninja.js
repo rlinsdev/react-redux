7e94cb266edc6e460320ac55499333ef641350ff
Rodrigo Lins e Freitas <rlinsdev@gmail.com>
2020-10-17 15:11:57 -0300

    First Module

---
+++ src/ninja.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Ninjas extends Component{
+    render(){
+        return <div className='ninja'>
+            <div>name: Lins</div>
+            <div>age: 38</div>
+            <div>belt: white</div>
+        </div>
+    }
+}
+
+export default Ninjas;
